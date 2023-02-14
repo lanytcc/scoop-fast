e7ab0add3113d5e33225d5f298a68819bdfed1a9
Aizaixyq <96584640+Aizaixyq@users.noreply.github.com>
2023-02-14 22:40:48 +0800

    fix search fun

--- xmake.lua
+++ xmake.lua
@@ -1,11 +1,12 @@
 add_rules("mode.debug", "mode.release")
 add_requires("fmt", "nlohmann_json")
+if is_mode("debug") then
+    add_defines("DEBUG")
+end
 
 target("sf")
     set_kind("binary")
     add_files("src/*.cpp")
     set_languages("cxx20")
     set_optimize("fastest")
-    is_mode("debug")
-        add_defines("DEBUG")
     add_packages("fmt", "nlohmann_json")
